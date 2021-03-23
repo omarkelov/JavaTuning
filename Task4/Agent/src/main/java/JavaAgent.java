9ef4527fb502e79b64e10486c5f38577eb9aefc7
omarkelov <36790281+omarkelov@users.noreply.github.com>
2021-03-23 11:38:47 +0700

    Add shutdown hook

--- Task4/Agent/src/main/java/JavaAgent.java
+++ Task4/Agent/src/main/java/JavaAgent.java
@@ -3,11 +3,12 @@ import java.lang.instrument.UnmodifiableClassException;
 
 public class JavaAgent {
     public static void premain(String agentArgs, Instrumentation instrumentation) throws ClassNotFoundException, UnmodifiableClassException {
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+            System.out.println("Loaded classes: " + instrumentation.getAllLoadedClasses().length)));
+
         Class<?> clazz = Class.forName("TransactionProcessor");
 
         instrumentation.addTransformer(new TransactionProcessorTransformer(clazz), true);
         instrumentation.retransformClasses(clazz);
-
-        System.out.println("Loaded classes: " + instrumentation.getAllLoadedClasses().length);
     }
 }
